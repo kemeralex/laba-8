b784c70d49ecea9c226749e0e1adde2558955170
Александр <@DESKTOP-MONEL6H>
2024-04-19 00:45:07 +0700

    Добавьте файлы проекта.

---
+++ laba 8/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+public interface ITriangle
+{
+    double GetArea();
+    double GetPerimeter();
+}
+
+public abstract class Polygon : ITriangle
+{
+    public List<double> Sides { get; set; }
+
+    public Polygon(params double[] sides)
+    {
+        Sides = new List<double>(sides);
+    }
+
+    public abstract double GetArea();
+
+    public double GetPerimeter()
+    {
+        return Sides.Sum();
+    }
+}
+
+public class Triangle : Polygon
+{
+    public double Angle1 { get; set; }
+    public double Angle2 { get; set; }
+    public double Angle3 { get; set; }
+
+    public Triangle(double side1, double side2, double side3, double angle1, double angle2, double angle3) :
+        base(side1, side2, side3)
+    {
+        Angle1 = angle1;
+        Angle2 = angle2;
+        Angle3 = angle3;
+    }
+
+    public override double GetArea()
+    {
+        return 0.5 * Sides[0] * Sides[1] * Math.Sin(Angle1 * Math.PI / 180);
+    }
+
+    public double GetHeight(int sideIndex)
+    {
+        return 2 * GetArea() / Sides[sideIndex];
+    }
+
+    public string GetTriangleType()
+    {
+        if (Angle1 == 90 || Angle2 == 90 || Angle3 == 90)
+        {
+            return "Прямоугольный";
+        }
+        else if (Sides[0] == Sides[1] && Sides[1] == Sides[2])
+        {
+            return "Равносторонний";
+        }
+        else if (Sides[0] == Sides[1] || Sides[1] == Sides[2] || Sides[0] == Sides[2])
+        {
+            return "Равнобедренный";
+        }
+        else
+        {
+            return "Произвольный";
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" Введите значения сторон и углов: ");
+
+        double AB = (double)Convert.ToDouble(Console.ReadLine());
+        double BC = (double)Convert.ToDouble(Console.ReadLine());
+        double CA = (double)Convert.ToDouble(Console.ReadLine());
+        double angle1 = (double)Convert.ToDouble(Console.ReadLine());
+        double angle2 = (double)Convert.ToDouble(Console.ReadLine());
+        double angle3 = (double)Convert.ToDouble(Console.ReadLine());
+
+        // Данные о треугольнике
+        Triangle triangle = new Triangle(AB, BC, CA, angle1, angle2, angle3);
+        Console.WriteLine("Данные о треугольнике:");
+        Console.WriteLine($"Площадь: {triangle.GetArea()}");
+        Console.WriteLine($"Периметр: {triangle.GetPerimeter()}");
+        Console.WriteLine($"Длина, проведенная с первое стороны: {triangle.GetHeight(0)}");
+        Console.WriteLine($"Длина, проведенная со второй стороны: {triangle.GetHeight(1)}");
+        Console.WriteLine($"Длина, проведенная с третьей стороны: {triangle.GetHeight(2)}");
+        Console.WriteLine($"Тип треугольника: {triangle.GetTriangleType()}");
+
+        // Изменение значений или конец программы
+        while (true)
+        {
+            Console.WriteLine("Введите 'с', чтобы изменить значения сторон, 'у' - изменить значения углов, 'в' - выйти из программы:");
+            string input = Console.ReadLine();
+
+            if (input == "с")
+            {
+                Console.WriteLine("Введите новые значения сторон (через пробел):");
+                double[] newSides = Array.ConvertAll(Console.ReadLine().Split(' '), double.Parse);
+                triangle.Sides = new List<double>(newSides); 
+            }
+            else if (input == "у")
+            {
+                Console.WriteLine("Введите новый значения углов (через пробел):");
+                double[] newAngles = Array.ConvertAll(Console.ReadLine().Split(' '), double.Parse);
+                triangle.Angle1 = newAngles[0];
+                triangle.Angle2 = newAngles[1];
+                triangle.Angle3 = newAngles[2];
+            }
+            else if (input == "в")
+            {
+                break;
+            }
+
+            // Измененные данные о треугольнике
+            Console.WriteLine("Данные о треугольнике:");
+            Console.WriteLine($"Площадь: {triangle.GetArea()}");
+            Console.WriteLine($"Периметр: {triangle.GetPerimeter()}");
+            Console.WriteLine($"Длина, проведенная с первое стороны: {triangle.GetHeight(0)}");
+            Console.WriteLine($"Длина, проведенная со второй стороны: {triangle.GetHeight(1)}");
+            Console.WriteLine($"Длина, проведенная с третьей стороны: {triangle.GetHeight(2)}");
+            Console.WriteLine($"Тип треугольника: {triangle.GetTriangleType()}");
+        }
+    }
+}
---
+++ вариант 2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+public abstract class BitStringBase
+{
+    public abstract ulong High { get; set; }
+    public abstract ulong Low { get; set; }
+
+    public abstract BitStringBase And(BitStringBase other);
+    public abstract BitStringBase Or(BitStringBase other);
+    public abstract BitStringBase Xor(BitStringBase other);
+    public abstract BitStringBase Not();
+    public abstract BitStringBase ShiftLeft(int count);
+    public abstract BitStringBase ShiftRight(int count);
+
+    public override string ToString()
+    {
+        return $"{High:X16}{Low:X16}";
+    }
+}
+
+public class BitString : BitStringBase
+{
+    private ulong _high;
+    private ulong _low;
+
+    public BitString(ulong high, ulong low)
+    {
+        _high = high;
+        _low = low;
+    }
+
+    public override ulong High { get { return _high; } set { _high = value; } }
+    public override ulong Low { get { return _low; } set { _low = value; } }
+
+    public override BitStringBase And(BitStringBase other)
+    {
+        BitString otherBitString = (BitString)other;
+        return new BitString(_high & otherBitString._high, _low & otherBitString._low);
+    }
+
+    public override BitStringBase Or(BitStringBase other)
+    {
+        BitString otherBitString = (BitString)other;
+        return new BitString(_high | otherBitString._high, _low | otherBitString._low);
+    }
+
+    public override BitStringBase Xor(BitStringBase other)
+    {
+        BitString otherBitString = (BitString)other;
+        return new BitString(_high ^ otherBitString._high, _low ^ otherBitString._low);
+    }
+
+    public override BitStringBase Not()
+    {
+        return new BitString(~_high, ~_low);
+    }
+
+    public override BitStringBase ShiftLeft(int count)
+    {
+        if (count < 64)
+        {
+            return new BitString(_high << count | _low >> (64 - count), _low << count);
+        }
+        else if (count == 64)
+        {
+            return new BitString(_low, 0);
+        }
+        else
+        {
+            return new BitString(0, 0);
+        }
+    }
+
+    public override BitStringBase ShiftRight(int count)
+    {
+        if (count < 64)
+        {
+            return new BitString(_high >> count, _low >> count | _high << (64 - count));
+        }
+        else if (count == 64)
+        {
+            return new BitString(0, _high);
+        }
+        else
+        {
+            return new BitString(0, 0);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Get bit string data from user
+        Console.WriteLine("Enter the high and low 64-bit values for the bit string (separated by spaces):");
+        string[] input = Console.ReadLine().Split(' ');
+        ulong high = ulong.Parse(input[0], System.Globalization.NumberStyles.HexNumber);
+        ulong low = ulong.Parse(input[1], System.Globalization.NumberStyles.HexNumber);
+
+        // Create a bit string object
+        BitString bs = new BitString(high, low);
+
+        // Print the bit string
+        Console.WriteLine("Bit string:");
+        Console.WriteLine($"High: {bs.High:X16}");
+        Console.WriteLine($"Low: {bs.Low:X16}");
+
+        // Get shift direction and amount from user
+        Console.WriteLine("Enter 'l' to shift left or 'r' to shift right:");
+        string direction = Console.ReadLine();
+
+        Console.WriteLine("Enter the number of bits to shift:");
+        int count = int.Parse(Console.ReadLine());
+
+        // Perform the shift operation
+        BitStringBase result;
+        if (direction == "l")
+        {
+            result = bs.ShiftLeft(count);
+        }
+        else if (direction == "r")
+        {
+            result = bs.ShiftRight(count);
+        }
+        else
+        {
+            Console.WriteLine("Invalid direction");
+            return;
+        }
+
+        // Print the result
+        Console.WriteLine("Result:");
+        Console.WriteLine($"High: {result.High:X16}");
+        Console.WriteLine($"Low: {result.Low:X16}");
+    }
+}
